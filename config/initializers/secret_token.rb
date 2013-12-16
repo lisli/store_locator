e51a847dee10512875e08c5d3f8793ae2ddd999f
Lisa Yoder <lisa.alane.yoder@gmail.com>
2013-12-16 13:15:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GCTStoreLocator::Application.config.secret_key_base = '71a61514e0f12291ea720383eb464ca9b86e2f9f804064f67996d25b2218f9f4741838263720a06846606f394e6cee81e88128c694403314d3b60de042c780d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GCTStoreLocator::Application.config.session_store :cookie_store, key: '_GCT_Store_Locator_session'
